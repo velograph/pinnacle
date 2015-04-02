0ed59319a24debb09c51dd03d1933cb3dd46c443
Jason Britton <velograph@gmail.com>
2015-04-02 15:37:24 -0700

    Install Typekit font.

--- functions.php
+++ functions.php
@@ -110,3 +110,20 @@ require get_template_directory() . '/inc/extras.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * TypeKit Fonts
+ *
+ * @since Theme 1.0
+ */
+function theme_typekit() {
+    wp_enqueue_script( 'theme_typekit', '//use.typekit.net/slm1sim.js');
+}
+add_action( 'wp_enqueue_scripts', 'theme_typekit' );
+
+function theme_typekit_inline() {
+  if ( wp_script_is( 'theme_typekit', 'done' ) ) { ?>
+  	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+<?php }
+}
+add_action( 'wp_head', 'theme_typekit_inline' );
