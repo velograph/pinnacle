8449543c5a739afda014fc6359fcaeb33d626273
Jason Britton <velograph@gmail.com>
2015-05-13 08:52:30 -0700

    Add work taxonomy

--- functions.php
+++ functions.php
@@ -211,6 +211,44 @@ function work_custom_post_type() {
 // Hook into the 'init' action
 add_action( 'init', 'work_custom_post_type', 0 );
 
+// Register Custom Taxonomy
+function register_work_taxonomy() {
+
+	$labels = array(
+		'name'                       => _x( 'Work Categories', 'Taxonomy General Name', 'text_domain' ),
+		'singular_name'              => _x( 'Work Category', 'Taxonomy Singular Name', 'text_domain' ),
+		'menu_name'                  => __( 'Work Categories', 'text_domain' ),
+		'all_items'                  => __( 'All Work Categories', 'text_domain' ),
+		'parent_item'                => __( 'Parent Work Category', 'text_domain' ),
+		'parent_item_colon'          => __( 'Parent Work Category:', 'text_domain' ),
+		'new_item_name'              => __( 'New Work Category', 'text_domain' ),
+		'add_new_item'               => __( 'Add New Work Category', 'text_domain' ),
+		'edit_item'                  => __( 'Edit Work Category', 'text_domain' ),
+		'update_item'                => __( 'Update Work Category', 'text_domain' ),
+		'view_item'                  => __( 'View Work Category', 'text_domain' ),
+		'separate_items_with_commas' => __( 'Separate roles with commas', 'text_domain' ),
+		'add_or_remove_items'        => __( 'Add or remove work categories', 'text_domain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+		'popular_items'              => __( 'Popular Work Categories', 'text_domain' ),
+		'search_items'               => __( 'Search Work Categories', 'text_domain' ),
+		'not_found'                  => __( 'Not Found', 'text_domain' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'work-categories', array( 'work' ), $args );
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'register_work_taxonomy', 0 );
+
 function video_custom_post_type() {
 
 	$labels = array(
